96d2a753fd281eb7aceef95bccdf6ab794fd5268
Doğukan Ürker <dogukanurker@icloud.com>
2023-12-20 22:13:16 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,8 +1,2 @@
 account_sid = "ACd343473d794bb7afea2f149fc4d18de5"
-auth_token = "0e18b7c5846422040ee94f011d71e41f"
-
-import phonenumbers
-from phonenumbers import carrier
-from phonenumbers.phonenumberutil import number_type
-
-print(carrier._is_mobile(number_type(phonenumbers.parse("+90 551 185 85 36"))))
+auth_token = "5fa85b83e7d3890ffe18621a2dc72d34"

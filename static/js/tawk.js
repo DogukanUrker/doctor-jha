b49df4d49c8d546ee2323fd758e0b0e07170be47
Doğukan Ürker <dogukanurker@icloud.com>
2023-12-25 21:35:08 +0300

    tawk.js added

---
+++ static/js/tawk.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/6589cb0f70c9f2407f8343a9/1hih42d16";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
